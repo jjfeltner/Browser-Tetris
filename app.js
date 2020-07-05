27b7f47948299c52fb1d4c9d89c00f329efdfb74
jjfeltner <jaredjfeltner@gmail.com>
2020-07-04 21:46:15 -0400

    Added seven tetrinos (J, L, O, S, Z, T, I)

--- app.js
+++ app.js
@@ -1,7 +1,59 @@
-document.addEventListener('DOMContentLoaded'() => {
-  //code goes here
+document.addEventListener('DOMContentLoaded', () => {
+  const grid= document.querySelector('.grid')
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const ScoreDisplay = document.querySelector('#score')
+  const StartBtn = document.querySelector('#start-button')
+  const width = 10
 
+  //The Tetrominoes
+  //resume 37:41
+  const jTetromino = [
+    [1, width+1, width*2, width*2+1],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, 2],
+    [0, width, width+1, width+2]
+  ]
 
+  const lTetromino = [
+    [1, width+1, width*2+1, width*2+2],
+    [width, width+1, 2, width+2],
+    [0, 1, width+1, width*2+1],
+    [width, width*2, width+1, width+2]
+  ]
 
+  const oTetromino = [
+    [0, width, 1, width+1],
+    [0, width, 1, width+1],
+    [0, width, 1, width+1],
+    [0, width, 1, width+1]
+  ]
+
+  const sTetromino = [
+    [width, 1, width+1, 2],
+    [0, width, width+1, width*2+1],
+    [width, 1, width+1, 2],
+    [0, width, width+1, width*2+1]
+  ]
+
+  const zTetromino = [
+    [0, 1, width+1, width+2],
+    [width+1, width*2+1, 2, width+2],
+    [0, 1, width+1, width+2],
+    [width+1, width*2+1, 2, width+2]
+  ]
+
+  const tTetromino = [
+    [0, 1, width+1, 2],
+    [0, width, width*2, width+1],
+    [width*2, width+1, width*2+1, width*2+2],
+    [width+1, 2, width+2, width*2+2]
+  ]
+
+  const iTetromino = [
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3],
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3]
+  ]
 
 })
